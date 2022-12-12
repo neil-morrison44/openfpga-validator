557a754943910eea3cbb35d127a639b816a4ad54
Neil Morrison <email@neil.today>
2022-12-12 22:53:54 +0000

    Updates bad test snapshot

--- tests/bad.test.ts
+++ tests/bad.test.ts
@@ -15,22 +15,22 @@ test("bad.zip", async () => {
 "Error: Unparsable JSON file: this_isnt.json SyntaxError: Unexpected token h in JSON at position 1
 Error: Core folder Cores/AuthorName.coreName should be Cores/SomeoneElse.very very very very very long short name to match 
 https://www.analogue.co/developer/docs/core-definition-files#folder-naming-convention
-Error: Missing / incorrect \`core.magic\` in Cores/AuthorName.coreName/core.json 
-https://www.analogue.co/developer/docs/core-definition-files/core-json#json-definition
-Error: framework.target_product must be set to "Analogue Pocket" in Cores/AuthorName.coreName/core.json 
-https://www.analogue.co/developer/docs/core-definition-files/core-json#framework
-Error: missing / incorrect hardware.cartridge_adapter in Cores/AuthorName.coreName/core.json 
-https://www.analogue.co/developer/docs/core-definition-files/core-json#hardware
-Error: param shortname too long (40 > 31) in Cores/AuthorName.coreName/core.json 
-https://www.analogue.co/developer/docs/core-definition-files/core-json#cores
-Error: param description too long (92 > 63) in Cores/AuthorName.coreName/core.json 
-https://www.analogue.co/developer/docs/core-definition-files/core-json#cores
-Error: param url too long (94 > 63) in Cores/AuthorName.coreName/core.json 
-https://www.analogue.co/developer/docs/core-definition-files/core-json#cores
-Error: param version too long (36 > 31) in Cores/AuthorName.coreName/core.json 
-https://www.analogue.co/developer/docs/core-definition-files/core-json#cores
-Error: param date_release too long (19 > 10) in Cores/AuthorName.coreName/core.json 
-https://www.analogue.co/developer/docs/core-definition-files/core-json#cores
+Error: Cores/AuthorName.coreName/core.json invalid: 
+
+https://www.analogue.co/developer/docs/core-definition-files/core-json
+
+Error: core.magic Invalid literal value, expected "APF_VER_1"
+Error: core.metadata.platform_ids Array must contain at most 4 element(s)
+Error: core.metadata.shortname String must contain at most 31 character(s)
+Error: core.metadata.description String must contain at most 63 character(s)
+Error: core.metadata.url String must contain at most 63 character(s)
+Error: core.metadata.version String must contain at most 31 character(s)
+Error: core.metadata.date_release String must contain at most 10 character(s)
+Error: core.framework.target_product Invalid literal value, expected "Analogue Pocket"
+Error: core.framework.chip32_vm String must contain at most 15 character(s)
+Error: core.framework.hardware.cartridge_adapter Invalid input
+Error: core.cores.0.name String must contain at most 15 character(s)
+Error: core.cores.0.filename String must contain at most 15 character(s)
 Recommendation: SemVer versioning is highly encouraged - \`not-semver-and-also-far-far-too-long\` in Cores/AuthorName.coreName/core.json 
 https://www.analogue.co/developer/docs/core-definition-files/core-json#metadata
 Error: Specified platform nonsense_1 is missing, Platforms/nonsense_1.json should exist 
@@ -69,7 +69,7 @@ Error: Specified platform nonsense_9 is missing, Platforms/nonsense_9.json shoul
  https://www.analogue.co/developer/docs/platform-metadata#platform.json
 Recommendation: nonsense_9 is missing an image 
  https://www.analogue.co/developer/docs/platform-metadata#platform-image
-Exit code 19
+Exit code 24
 "
 `)
 })
